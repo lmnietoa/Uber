0798eaafd130465d8305c3e3241dbfb7b780b908
LinaNieto <lmnietoa@gmail.com>
2023-03-28 12:25:40 -0500

    método constructor

--- Java/Car.java
+++ Java/Car.java
@@ -1,12 +1,15 @@
-import java.time.LocalDate;
-
 public class Car {
     Integer id;
     String license;
-    String driver;
+    Account driver;
     Integer passenger;
 
+    public Car(String license, Account driver) {
+        this.license = license;
+        this.driver = driver;
+    }
+
     void printDataCar() {
-        System.out.println("License: " + license + " Driver: " + driver);
+        System.out.println("Placa: " + license + " Conductor: " + driver.name);
     }
 }
