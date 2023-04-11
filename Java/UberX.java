4119bf6fb4a467f1a777e7ce84c1590c33a923d1
LinaNieto <lmnietoa@gmail.com>
2023-04-11 13:47:13 -0500

    Herencia

---
+++ Java/UberX.java
@@ -0,0 +1,10 @@
+class UberX extends Car {
+    String brand;
+    String model;
+
+    public UberX(String license, Account driver, String brand, String model){
+        super/*super representa a la superclase*/(license, driver)
+        this/*Representa la clase hija */.brand = brand;
+        this.model = model;
+    }
+}
