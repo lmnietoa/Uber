693c757222a2cd86cb92d8b15f3c9ab3decc4056
LinaNieto <lmnietoa@gmail.com>
2023-04-18 10:51:37 -0500

    Herencia en JS

--- JS/index.js
+++ JS/index.js
@@ -2,5 +2,7 @@ var car = new Car("TJO30E", new Account("Lina Nieto", "LIN123"))
 car.passenger = 4;
 car.printDataCar();
 
-
+var uberX = new UberX("AW456", new Account("Andrea Ferran", "ANDA765"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
 
